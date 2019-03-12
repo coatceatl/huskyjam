dfdfa8efc263e4201505c642365f6a10bec2a3f2
Oksana Tkachenko <oksana@crystax.net>
2019-03-12 18:11:24 +0200

    added slider swiper

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+});
